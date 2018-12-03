f398ccce1b6535ee666d7f983b6368ce5f805e2e
Jose Pacheco <jap@hpe.com>
2018-12-03 17:59:31 +0000

    Implemented last assertions

--- ProyI/sdram_agent/wh_ifc.svh
+++ ProyI/sdram_agent/wh_ifc.svh
@@ -7,14 +7,29 @@
 
 `timescale 1ns/1ps
 interface wh_ifc;
+    //Whitebox
     logic clk_i;
     logic rst_i;
     logic cyc_i;
     logic stb_i;
     logic ack_o;
 
+    //IFC signals
+    logic            ifc_stb_i;
+    logic            ifc_ack_o;
+    logic [25:0]     ifc_addr_i;
+    logic            ifc_we_i;
+    logic [32-1:0]   ifc_dat_i;
+    logic [32/8-1:0] ifc_sel_i;
+    logic [32-1:0]   ifc_dat_o;
+    logic            ifc_cyc_i;
+    logic  [2:0]     ifc_cti_i;
+
+    //Assertions
     property wb300;
-        @(posedge clk_i) 1; //What does it mean by INITIALIZED STATE?
+        @(posedge clk_i) $rose(rst_i) |->
+           (ifc_stb_i == 0 && ifc_ack_o == 0 && ifc_addr_i == 0 && ifc_we_i  == 0 && ifc_dat_i == 0 &&
+            ifc_sel_i == 0 && ifc_dat_o == 0 && ifc_cyc_i  == 0 && ifc_cti_i == 0) throughout (rst_i);
     endproperty
     wb300_asrt: assert property (wb300) else $display("[SDRAM_ASSRT] Property wb300 has failed");
     wb300_covr: cover property (wb300) $display("[SDRAM_ASSRT] Property wb300 has passed");
@@ -26,7 +41,9 @@ interface wh_ifc;
     wb305_covr: cover property (wb305) $display("[SDRAM_ASSRT] Property wb305 has passed");
 
     property wb310;
-        @(posedge clk_i) 1; //What does it mean by reacting?
+        @(posedge clk_i) not (rst_i == 1 && (
+           ifc_stb_i == 1 || ifc_ack_o == 1 || ifc_addr_i == 1 || ifc_we_i  == 1 || ifc_dat_i == 1 ||
+           ifc_sel_i == 1 || ifc_dat_o == 1 || ifc_cyc_i  == 1 || ifc_cti_i == 1)); //Done. Interface reacts by going to initialized state
     endproperty
     wb310_asrt: assert property (wb310) else $display("[SDRAM_ASSRT] Property wb310 has failed");
     wb310_covr: cover property (wb310) $display("[SDRAM_ASSRT] Property wb310 has passed");
